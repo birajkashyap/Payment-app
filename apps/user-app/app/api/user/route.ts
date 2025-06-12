061841d51a2abcf5333982cbfde0a841b2c0896c
Biraj Kashyap <kashyapbiraj83@gmail.com>
2025-06-12 22:33:42 +0530

    day2 changes

---
+++ apps/user-app/app/api/user/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@repo/db/generated/prisma";
+
+const client = new PrismaClient();
+
+export const GET = () => {
+  return NextResponse.json({
+    message: "hi there",
+  });
+};
+
+// export const POST = async (req: NextRequest, res: NextResponse) => {
+//   const body = await req.json();
+//   const { name, email } = body;
+//   await client.user.create({
+//     data: {
+//       name: name,
+//       email: email,
+//     },
+//   });
+// };
